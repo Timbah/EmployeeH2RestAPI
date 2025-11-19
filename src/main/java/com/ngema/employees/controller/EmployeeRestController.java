eb350ca2f2e3a37eb84c9159c17f2b0d00822579
Thembelani Ngema <thembelaningema20@gmail.com>
2025-11-19 20:22:28 +0200

    Added Controller Methods - Find all, get single employee

--- src/main/java/com/ngema/employees/controller/EmployeeRestController.java
+++ src/main/java/com/ngema/employees/controller/EmployeeRestController.java
@@ -2,15 +2,18 @@ package com.ngema.employees.controller;
 
 import com.ngema.employees.entity.Employee;
 import com.ngema.employees.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
+@Tag(name = "Employee Rest API Endpoints", description = "Operations related to employees.")
 public class EmployeeRestController {
 
     private EmployeeService employeeService;
@@ -20,8 +23,21 @@ public class EmployeeRestController {
         this.employeeService = employeeService;
     }
 
+    @Operation(summary = "Get a list of all employees", description = "Retrieve a list of all employees")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping()
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    @Operation(summary = "Fetch a single employee", description = "Get a single employee from database")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{employeeId}")
+    public Employee getEmployee(@PathVariable @Min(value = 1) long employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+        return theEmployee;
+    }
+
+
 }
