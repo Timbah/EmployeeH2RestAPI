97a7181a6fe47ac94e30dc7cc48d5e37f338b7fd
Thembelani Ngema <thembelaningema20@gmail.com>
2025-11-19 11:43:31 +0200

    Added Service methods:Find,Add,Update & Delete

--- src/main/java/com/ngema/employees/service/EmployeeService.java
+++ src/main/java/com/ngema/employees/service/EmployeeService.java
@@ -1,10 +1,21 @@
 package com.ngema.employees.service;
 
 import com.ngema.employees.entity.Employee;
+import com.ngema.employees.request.EmployeeRequest;
 
 import java.util.List;
 
 public interface EmployeeService {
 
     List<Employee> findAll();
+
+    Employee findById(long theId);
+
+    Employee save(EmployeeRequest employeeRequest);
+
+    Employee update(long id, EmployeeRequest employeeRequest);
+
+    Employee convertToEmployee(long id, EmployeeRequest employeeRequest);
+
+    void deleteById(long theId);
 }
--- src/main/java/com/ngema/employees/service/EmployeeServiceImpl.java
+++ src/main/java/com/ngema/employees/service/EmployeeServiceImpl.java
@@ -2,8 +2,10 @@ package com.ngema.employees.service;
 
 import com.ngema.employees.dao.EmployeeDAO;
 import com.ngema.employees.entity.Employee;
+import com.ngema.employees.request.EmployeeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +23,42 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(long theId) {
+
+        Employee employee = employeeDAO.findById(theId);
+        return employee;
+    }
+
+    @Transactional
+    @Override
+    public Employee save(EmployeeRequest employeeRequest) {
+
+        Employee employee = convertToEmployee(0, employeeRequest);
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public Employee update(long id, EmployeeRequest employeeRequest) {
+
+        Employee employee = convertToEmployee(id, employeeRequest);
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    public Employee convertToEmployee(long id, EmployeeRequest employeeRequest) {
+
+        return new Employee(id, employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getEmail());
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long theId) {
+        employeeDAO.deleteById(theId);
     }
 }
