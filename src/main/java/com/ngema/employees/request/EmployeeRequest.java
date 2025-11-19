b69994a0ec2f7bda23e00dd3db8f95b95d773bc4
Thembelani Ngema <thembelaningema20@gmail.com>
2025-11-19 11:58:20 +0200

    Added validations on the employee request class and added swagger config

--- src/main/java/com/ngema/employees/request/EmployeeRequest.java
+++ src/main/java/com/ngema/employees/request/EmployeeRequest.java
@@ -1,9 +1,21 @@
 package com.ngema.employees.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeRequest {
 
+    @NotBlank(message = "First Name is mandatory")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
+
+    @NotBlank(message = "First Name is mandatory")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String lastName;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Please provide a valid email address")
     private String email;
 
     public EmployeeRequest(String firstName, String lastName, String email) {
